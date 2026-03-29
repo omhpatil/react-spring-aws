0f8f26f1d8ce9c722e742bfe634d078a785cb4fc
Om Patil <97589129+omhpatil@users.noreply.github.com>
2026-03-29 15:14:41 +0530

    Update api.js

--- library-frontend/src/api.js
+++ library-frontend/src/api.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
-const BASE_URL = "/books"; // Change if needed
+const BASE_URL = "/api/books";
 
 export const getBooks = () => axios.get(BASE_URL);
+
 export const getBook = (id) => axios.get(`${BASE_URL}/${id}`);
+
 export const addBook = (book) => axios.post(BASE_URL, book);
+
 export const deleteBook = (id) => axios.delete(`${BASE_URL}/${id}`);
+
 export const updateAvailability = (id, available) =>
   axios.patch(`${BASE_URL}/${id}`, { available });
