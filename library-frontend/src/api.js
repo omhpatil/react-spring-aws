c5e10f3e01e3249595631c4a93c3c0f2276524e7
Om Patil <97589129+omhpatil@users.noreply.github.com>
2026-04-08 00:06:25 +0530

    Update api.js

--- library-frontend/src/api.js
+++ library-frontend/src/api.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = "/api/books";
-<<<<<<< HEAD
+
 // const BASE_URL = "http://localhost:8080/books"; // Change if needed for backend
-=======
->>>>>>> 0f8f26f1d8ce9c722e742bfe634d078a785cb4fc
 
 export const getBooks = () => axios.get(BASE_URL);
 
